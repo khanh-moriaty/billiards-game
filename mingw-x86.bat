4d191c2f74cce2ec890d910a0fde2f33ab90c23e
moriaty <19520624@gm.uit.edu.vn>
2021-06-01 22:54:35 +0700

    Added compatible x86 and x86-64 makefile

---
+++ mingw-x86.bat
@@ -0,0 +1,1 @@
+gcc -w -std=c++0x -I. -I GLEW/include/ -I GLFW/include/ -I GLM/include/ -I SOIL2/include/ -o main.exe ./Application.cpp ./GLM/include/glm/detail/glm.cpp ./SOIL2/include/SOIL2.c ./SOIL2/include/image_DXT.c ./SOIL2/include/image_helper.c ./SOIL2/include/wfETC.c -L. -lglfw3_x86 -lglew32_x86 -lopengl32 -lstdc++  
