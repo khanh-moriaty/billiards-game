bfadaa4e672027a1bcd20701f8aa415b153d6943
moriaty <19520624@gm.uit.edu.vn>
2021-06-01 23:02:28 +0700

    Fixed typo

--- mingw-x86.bat
+++ mingw-x86.bat
@@ -1,3 +1,3 @@
-cp glfw3_x86.dll glfw3.dll
-cp glew32_x86.dll glew32.dll
+copy glfw3_x86.dll glfw3.dll
+copy glew32_x86.dll glew32.dll
 gcc -w -std=c++0x -I. -I GLEW/include/ -I GLFW/include/ -I GLM/include/ -I SOIL2/include/ -o main.exe ./Application.cpp ./GLM/include/glm/detail/glm.cpp ./SOIL2/include/SOIL2.c ./SOIL2/include/image_DXT.c ./SOIL2/include/image_helper.c ./SOIL2/include/wfETC.c -L. -lglfw3 -lglew32 -lopengl32 -lstdc++  
