c94e28d82efc43bfa3a9407d26965602b971155a
moriaty <19520624@gm.uit.edu.vn>
2021-06-02 11:55:25 +0700

    Added install script for win64. Removed old install scripts.

--- install_win64.bat
+++ install_win64.bat
@@ -5,7 +5,7 @@ gcc -DGLEW_NO_GLU -O2 -w -I include -DGLEW_BUILD -o src/glew.o -c src/glew.c
 gcc -nostdlib -shared -Wl,-soname,libglew32.dll -Wl,--out-implib,lib/libglew32.dll.a -o lib/glew32.dll src/glew.o -lglu32 -lopengl32 -lgdi32 -luser32 -lkernel32
 cd ..\
 copy GLEW\lib\glew32.dll .\
-copy GLFW\lib\glfw3_x86.dll .\glfw3.dll
+copy GLFW\lib\glfw3_x86-64.dll .\glfw3.dll
 
 gcc -w -std=c++0x -I. -I GLEW/include/ -I GLFW/include/ -I GLM/include/ -I SOIL2/include/ -o main.exe ^
 ./Application.cpp ./GLM/include/glm/detail/glm.cpp ./SOIL2/include/SOIL2.c ./SOIL2/include/image_DXT.c ./SOIL2/include/image_helper.c ./SOIL2/include/wfETC.c ^
