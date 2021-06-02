0d061f4657881c8d5cebb38ec428c1b60c6bfc06
moriaty <19520624@gm.uit.edu.vn>
2021-06-02 11:40:17 +0700

    Suppress output for install script

--- install_win32.bat
+++ install_win32.bat
@@ -1,3 +1,4 @@
+@ECHO OFF
 cd GLEW
 mkdir lib
 gcc -DGLEW_NO_GLU -O2 -w -I include -DGLEW_BUILD -o src/glew.o -c src/glew.c
@@ -8,4 +9,3 @@ copy GLEW\lib\glew32.dll .\
 gcc -w -std=c++0x -I. -I GLEW/include/ -I GLFW/include/ -I GLM/include/ -I SOIL2/include/ -o main.exe ^
 ./Application.cpp ./GLM/include/glm/detail/glm.cpp ./SOIL2/include/SOIL2.c ./SOIL2/include/image_DXT.c ./SOIL2/include/image_helper.c ./SOIL2/include/wfETC.c ^
 -L. -lglfw3 -lglew32 -lopengl32 -lstdc++  
-
